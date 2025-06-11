e4b754aad424f7cde879ddff565fcf4703c26823
Ankur Banerjee <35483296-ankurbaner09@users.noreply.replit.com>
2025-06-11 16:43:13 +0000

    Week 1 work done. Note-- doesn't support xls files

--- backend.py
+++ backend.py
@@ -1,7 +1,14 @@
+import pandas as pd
+import openpyxl
+
 def handle_upload(request):
     excel_file = request.files.get('excel_file')
+    if excel_file:
+        df = pd.read_excel(excel_file)
+        print("Form Data Has Been Received!")
+    else:
+        return ("No Form Data Has Been Received!")
     user_question = request.form.get('user_question')
-    print("Form Data Has Been Received!")
     print(f"User Question: {user_question}")
 
     if excel_file:
@@ -10,4 +17,4 @@ def handle_upload(request):
     else:
         print("No file uploaded")
 
-    return f"<h2>Data Received!</h2><p>Question: {user_question if user_question else 'No Question Was Submitted'}</p><p>File: {excel_file.filename if excel_file else 'No File Was Submitted'}</p>"
+    return f"<h2>Data Received!</h2><p>Question: {user_question if user_question else 'No Question Was Submitted'}</p><p>File: {df.head().to_html(), excel_file.filename if excel_file else 'No File Was Submitted'}</p>"
